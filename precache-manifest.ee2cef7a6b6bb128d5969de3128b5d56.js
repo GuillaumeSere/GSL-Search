f145b10e49afc7123f5c4bf0f27effa8e289ce09
Guillaume Sere <guillaumesere@hotmail.fr>
2023-02-10 13:58:24 +0100

    Updates

---
+++ precache-manifest.ee2cef7a6b6bb128d5969de3128b5d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43646effd6f1c1ca8d54cb4cf1f3d07a",
+    "url": "/GSL-Search/index.html"
+  },
+  {
+    "revision": "51fc619028113ff09669",
+    "url": "/GSL-Search/static/css/main.585ae666.chunk.css"
+  },
+  {
+    "revision": "ecb48ab10ea3415872d9",
+    "url": "/GSL-Search/static/js/2.b97fd9a8.chunk.js"
+  },
+  {
+    "revision": "f5452f6ab2ae5c6f8f7a306e877f0e1f",
+    "url": "/GSL-Search/static/js/2.b97fd9a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51fc619028113ff09669",
+    "url": "/GSL-Search/static/js/main.1db3491e.chunk.js"
+  },
+  {
+    "revision": "4688314cca8e0d0c55ba",
+    "url": "/GSL-Search/static/js/runtime-main.e342137e.js"
+  }
+]);
