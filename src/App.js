d5b8105c96788857624f1a017f02b5877570b947
Guillaume Sere <guillaumesere@hotmail.fr>
2025-05-22 16:40:08 +0200

    modif router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Search from "./components/Search";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/search" exact element={<Search />} />
       </Routes>
-    </Router>
   );
 }
 
